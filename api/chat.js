1a1c130919b11ef0912d1a7475649f57515fc3da
AkbarJoon <arshiaqwe1234@gmail.com>
2026-08-20 16:08:14 +0330

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -59,7 +59,7 @@ async function fetchTavilyResults(query, tavilyKeys) {
     return null;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
